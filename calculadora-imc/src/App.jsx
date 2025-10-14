f378ae0c6524d4cc339562d97be16e9a1560ef0a
Anthony Xavier <anthonyltx12@gmail.com>
2025-10-14 10:46:12 -0300

    Criando componentes da calculadora

--- calculadora-imc/src/App.jsx
+++ calculadora-imc/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import ImcCalc from './components/ImcCalc'
 
 function App() {
 
   return (
-    <div className="App">
-      <p>IMC calc</p>
+    <div className="container">
+      <ImcCalc />
     </div>
   )
 }
---
+++ calculadora-imc/src/components/ImcCalc.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './ImcCalc.css'
+
+const ImcCalc = () => {
+  return (
+    <div id="calc-container">
+        <h2>Calculadora de IMC</h2>
+        <form id="imc-form">
+            <div className="from-inputs">
+                <div className="form-control">
+                    <label htmlFor="height">Altura:</label>
+                    <input 
+                    type="text" 
+                    name="height" 
+                    id="height" 
+                    placeholder="Exemplo 1,75" 
+                    />
+                </div>
+
+                <div className="form-control">
+                    <label htmlFor="wright">Peso:</label>
+                    <input 
+                    type="text" 
+                    name="wright" 
+                    id="wright" 
+                    placeholder="Exemplo 70" 
+                    />
+                </div>
+            </div>
+            <div className="action-control">
+                <button>Calcular</button>
+                <button>Limpar</button>
+            </div>
+        </form>
+
+    </div>
+  )
+}
+
+export default ImcCalc
